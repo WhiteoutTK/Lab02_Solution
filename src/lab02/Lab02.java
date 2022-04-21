0f76f42334231288e1e5043ae24d3e805ca4ff58
Samti <Samti@LAPTOP-RO0J975N>
2022-04-20 21:36:49 -0600

    Finished Version

---
+++ src/lab02/Lab02.java
@@ -0,0 +1,14 @@
+package lab02;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Lab02 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is the submission for lab 2");
+    }
+    
+}
